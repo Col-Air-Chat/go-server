6f322846a8758690b173a8081f5452b055409ca3
Okysu <yby@ecanse.com>
2023-01-18 23:27:43 +0800

    ➕ feat : this is the middleware of the timeout

---
+++ middleware/TimeoutMiddleware.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TimeoutMiddleware(timeout time.Duration) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c.Request.Context(), timeout)
+		defer func() {
+			if ctx.Err() == context.DeadlineExceeded {
+				c.Writer.WriteHeader(http.StatusGatewayTimeout)
+				c.Abort()
+			}
+			cancel()
+		}()
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
